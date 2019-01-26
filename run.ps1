f0b8abf152fae44d2107f4cd355bb6f62525dbe4
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-01-26 03:54:21 +0100

    Add more PHP setup

--- run.ps1
+++ run.ps1
@@ -1,13 +1,26 @@
 $cd = $PSScriptRoot
 
-$iisdir = "C:\Program Files (x86)\IIS Express"
 $phpdir = "$cd\php"
+
+# TODO : Copy php ini for dev to php.ini.
+# extension_dir
+# enable extensions mysqli, mbstring, mcrypt(?)
+# Follow recommendations here
+# Install wincache ? https://www.saotn.org/php-wincache-on-iis/
+# https://docs.microsoft.com/en-us/iis/application-frameworks/install-and-configure-php-on-iis/install-and-configure-mysql-for-php-applications-on-iis-7-and-above#configure-php-to-access-mysql
+
 $dbdir = "$cd\mariadb"
 
+
+# $conn = new mysqli('localhost', 'root', ''); if (mysqli_connect_errno()) { exit('Connect failed: '. mysqli_connect_error()); } $sql = 'CREATE DATABASE `wptest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci'; if ($conn->query($sql) === TRUE) { echo 'Database created successfully'; } else { echo 'Error creating database: ' . $conn->error; } $conn->close();
+
+$iisdir = "C:\Program Files (x86)\IIS Express"
 $appcmd = "$iisdir\appcmd.exe"
 
 if (!(Test-Path "$cd\applicationHost.config")) {
     Copy "$iisdir\AppServer\applicationHost.config" "applicationHost.config"
+
+    # TODO : Restart on php.ini changes
 
     & $appcmd "set" "config" "/section:system.webServer/fastCGI" "/+[fullPath='$phpdir\php-cgi.exe']" "/apphostconfig:""$cd\applicationHost.config"""
     & $appcmd "set" "config" "/section:system.webServer/handlers" "/+[name='PHP_via_FastCGI',path='*.php',verb='*',modules='FastCgiModule',scriptProcessor='$phpdir\php-cgi.exe',resourceType='Unspecified']" "/apphostconfig:""$cd\applicationHost.config"""
